79c5143ccb948abfd59b61dbed97e85482bfc312
Rendy Eza Putra <rendy.putra@binus.ac.id>
2022-04-24 18:04:11 +0700

    initial commit

---
+++ src/test/java/com/example/brickscrapper/BrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brickscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
